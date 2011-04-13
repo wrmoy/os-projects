9b5c0a088bd06e531f5a2d409d5651d5eee944e6
brian.dunlay@gmail.com <brian.dunlay@gmail.com@fd1b6a81-a21b-02b0-52b5-f319cd449e0a>
2011-04-13 07:12:49 +0000

    coolshellbro

--- proj1/shell.c
+++ proj1/shell.c
@@ -11,7 +11,42 @@ int main()
 	while(1){
 		printf("sish:> ");
 		fgets(cmd_buff, BUFF_MAX, stdin);
-		printf(" -- bleep bloop: %s", cmd_buff);
+	
+		// cmd_buff needs to be tokenized first, then
+		// the command parsed, and then the args dealt
+		// with accordingly
+	
+		// should also exit on ctrl+d, aka EOF
+		if (!strcmp(cmd_buff, "exit\n")) {
+			printf("Exit\n");
+		}
+		else if (!strcmp(cmd_buff, "ls\n")) {
+			printf("List\n");
+		}
+		else if (!strcmp(cmd_buff, "cat\n")) {
+			printf("Concatenate\n");
+		}
+		else if (!strcmp(cmd_buff, "pwd\n")) {
+			printf("Print working directory\n");
+		}	
+		else if (!strcmp(cmd_buff, "cd\n")) {
+			printf("Change directory\n");
+		}	
+		else if (!strcmp(cmd_buff, "rm\n")) {
+			printf("Remove\n");
+		}	
+		else if (!strcmp(cmd_buff, "mv\n")) {
+			printf("Move\n");
+		}	
+		else if (!strcmp(cmd_buff, "mkdir\n")) {
+			printf("Make Directory\n");
+		}	
+		else if (!strcmp(cmd_buff, "echo\n")) {
+			printf("Echo\n");
+		}	
+		else {
+			printf("-sish: command not found: %s", cmd_buff);
+		}	
 	}
 	
 	return 0;
