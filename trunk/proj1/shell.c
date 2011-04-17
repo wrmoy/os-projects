8eaf6531875a65ff20319983b3629418802da720
brian.dunlay@gmail.com <brian.dunlay@gmail.com@fd1b6a81-a21b-02b0-52b5-f319cd449e0a>
2011-04-17 23:22:54 +0000

    tokenizer added

--- trunk/proj1/shell.c
+++ trunk/proj1/shell.c
@@ -1,27 +1,28 @@
 /* Project 1 -- SISH -- Brian Dunlay / William Moy -- CS 170 W'11 */
 
 #include <stdio.h>
+#include <string.h>
 
 #define BUFF_MAX 1025 // 1024 + null terminating char
 
 int main()
 {
-	char cmd_buff[BUFF_MAX];
-	
+	char buffer[BUFF_MAX] = "";
 	while(1){
 		printf("sish:> ");
-		fgets(cmd_buff, BUFF_MAX, stdin);
-	
-		// cmd_buff needs to be tokenized first, then
-		// the command parsed, and then the args dealt
-		// with accordingly
-	
+		fgets(buffer, BUFF_MAX, stdin);
+
 		// should also exit on ctrl+d, aka EOF
-		if (!strcmp(cmd_buff, "exit\n")) {
+		if (!strcmp(buffer, "exit\n")) {
 			return 0;
 		}
+
+		char* token = strtok(buffer, "|");	
+		while(token != NULL) {
+			printf("%s ", token);
+			token = strtok(NULL, "|");
+		}
 	}
-	
 	return 0;
 }
 
