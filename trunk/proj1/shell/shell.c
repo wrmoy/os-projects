0d9bf5a2e6597d8a428ddee8ef071faee2d16519
william.r.moy@gmail.com <william.r.moy@gmail.com@fd1b6a81-a21b-02b0-52b5-f319cd449e0a>
2011-04-23 04:25:49 +0000

    more small changes, commands with pipes can be exec in sequence correctly, but piping is still not working

--- trunk/proj1/shell/shell.c
+++ trunk/proj1/shell/shell.c
@@ -23,19 +23,17 @@ int main()
 	char* tokens = malloc(sizeof(char)*BUFF_MAX);
 	char* rin_fname = malloc(sizeof(char)*ARGLEN_MAX);
 	char* rout_fname = malloc(sizeof(char)*ARGLEN_MAX);
-	//char* pipe_toks[2];
-	char** args = malloc(sizeof(char*)*ARG_MAX);//NULL;
-	memset(args, 0, 16);
+	char** args = NULL; //malloc(sizeof(char*)*ARG_MAX);
 	int i = 0;
-	//for (i=0; i < ARG_MAX; i++)
-	//	args[i] = malloc(sizeof(char)*ARGLEN_MAX);
+	int numargs = 0;
 	
 startloop:
 	while(1){
 		int background_flag = 0;
-		//int pipedcnt = 0;
+		int pipedcnt = 0;
 		int fdout = 0;
 		int fdin = 0;
+		numargs = 0;
 		
 		printf("sish:> ");
 		fgets(buffer, BUFF_MAX, stdin);
@@ -76,116 +74,144 @@ startloop:
 			}
 		}
 		
-			
-		/*
+		
 		// count the number of piped commands
-		strtok(buffer, "|");  pipedcnt++;
+		char* buffcopy = malloc(sizeof(char)*(strlen(buffer)+1));
+		strcpy(buffcopy, buffer);
+		strtok(buffcopy, "|");  pipedcnt++;
 		while(strtok(NULL, "|") != NULL)
 			pipedcnt++;
+		strcpy(buffcopy, buffer);
+
+		// create pipes
+		int** pfds = malloc(sizeof(int*)*pipedcnt-1);
+		for (i=0; i < pipedcnt-1; i++) {
+			pfds[i] = malloc(sizeof(int)*2);
+			pipe(pfds[i]);
+		}
 		
-		// divide line by pipes
-		pipe_toks[0] = strtok(buffer, "|");
+		// divide line by pipe chars
+		int procno;
+		pid_t lastpid;
+		int status;
+		char* pipe_toks[2];
+		pipe_toks[0] = strtok(buffcopy, "|");
 		pipe_toks[1] = strtok(NULL, "|");
-		for(i = 1; i < pipedcnt; i++) {
+		for(procno = 0; pipe_toks[0] != NULL; procno++) {
+			// prepare command
+			char* cmd = malloc(sizeof(char)*(strlen(pipe_toks[0])+1));
+			strcpy(cmd, pipe_toks[0]);
+			
+			// get redirects
+			if (procno == 0 && rinflag) {
+				// get the filename
+				char* tempstr;
+				tempstr = strtok(cmd, "<");
+				strcpy(cmd, tempstr);
+				tempstr = strtok(NULL, " \n\t");
+				strcpy(rin_fname, tempstr);
+				// copy the rest of the args back in
+				tempstr = strtok(NULL, " \n\t");
+				while (tempstr != NULL) {
+					strcat(cmd, " ");
+					strcat(cmd, tempstr);
+					tempstr = strtok(NULL, " \n\t");
+				}
+			}
+			if (procno == pipedcnt-1 && routflag) {
+				// get the filename
+				char* tempstr;
+				tempstr = strtok(cmd, ">");
+				strcpy(cmd, tempstr);
+				tempstr = strtok(NULL, " \n\t");
+				strcpy(rout_fname, tempstr);
+				// copy the rest of the args back in
+				tempstr = strtok(NULL, " \n\t");
+				while (tempstr != NULL) {
+					strcat(cmd, " ");
+					strcat(cmd, tempstr);
+					tempstr = strtok(NULL, " \n\t");
+				}
+			}
+		
+			// separate args
+			char* t = strtok(cmd, " \n\t");
+			for (numargs=0; t != NULL; numargs++) {
+				args = realloc(args, sizeof(char*)*(numargs+1));
+				args[numargs] = realloc(args[numargs], sizeof(char)*ARGLEN_MAX);
+				strcpy(args[numargs], t);
+				t = strtok(NULL, " \n\t");
+			}
+			args = realloc(args, sizeof(char*)*(numargs+1));
+			args[numargs] = NULL;
+			
+			printf("starting to fork proc%i\n", procno);
+			// fork it.  fork it all.
+			switch (lastpid = fork()) {
+				case 0:
+					if (pipedcnt > 1) {
+						printf("starting to redirect the pipes in proc%i\n", procno);
+						if (procno != pipedcnt-1) {
+							//close(STDOUT_FILENO);
+							//dup2(pfds[procno][1], STDOUT_FILENO);
+							//close(pfds[procno][1]);
+							printf("redirecting proc%i out\n", procno);
+						}
+						if (procno != 0) {
+							close(STDIN_FILENO);
+							dup2(pfds[procno-1][0], STDIN_FILENO);
+							close(pfds[procno-1][0]);
+							printf("redirecting proc%i in\n", procno);
+						}
+						printf("finished redirecting the pipes in proc%i\n", procno);
+						
+					}
+					if (procno == 0 && rinflag) {
+						fdin = open(rin_fname, O_RDONLY);
+						close(STDIN_FILENO);
+						dup2(fdin, STDIN_FILENO);
+						close(fdin);
+					}
+					if (procno == pipedcnt-1 && routflag) {
+						fdout = open(rout_fname, O_WRONLY | O_CREAT, 0644);
+						close(STDOUT_FILENO);
+						dup2(fdout, STDOUT_FILENO);
+						close(fdout);
+					}
+					execvp(args[0], args);
+					return 0;
+					break;
+				case -1:
+					printf("ERROR: could not create new process");
+					goto startloop;
+					break;
+				default:
+//					waitpid(lastpid, &status, 0); 
+					break;
+			}
+	
 			
 			// get new command sequence
 			pipe_toks[0] = pipe_toks[1];
 			pipe_toks[1] = strtok(NULL, "|");
-		}
-		*/
-		
-		
-		// get redirects
-		if (rinflag) {
-			// get the filename
-			char* tempstr;
-			tempstr = strtok(buffer, "<");
-			strcpy(buffer, tempstr);
-			tempstr = strtok(NULL, " \n\t");
-			strcpy(rin_fname, tempstr);
-			// copy the rest of the args back in
-			tempstr = strtok(NULL, " \n\t");
-			while (tempstr != NULL) {
-				strcat(buffer, " ");
-				strcat(buffer, tempstr);
-				tempstr = strtok(NULL, " \n\t");
-			}
-		}
-		if (routflag) {
-			// get the filename
-			char* tempstr;
-			tempstr = strtok(buffer, ">");
-			strcpy(buffer, tempstr);
-			tempstr = strtok(NULL, " \n\t");
-			strcpy(rout_fname, tempstr);
-			// copy the rest of the args back in
-			tempstr = strtok(NULL, " \n\t");
-			while (tempstr != NULL) {
-				strcat(buffer, " ");
-				strcat(buffer, tempstr);
-				tempstr = strtok(NULL, " \n\t");
-			}
+			free(cmd);
 		}
 		
-		// separate args
-		int numargs = 0;
-		char* t = strtok(buffer, " \n\t");
-		for (numargs=0; t != NULL; numargs++) {
-			args = realloc(args, sizeof(char*)*(numargs+1));
-			args[numargs] = malloc(sizeof(char)*ARGLEN_MAX);
-			strcpy(args[numargs], t);
-			t = strtok(NULL, " \n\t");
+		waitpid(lastpid, &status, 0);
+		for (i=0; i < pipedcnt-1; i++) {
+			free(pfds[i]);
 		}
-		
-		/* WTF */
-		
-		numargs++;
-		args = realloc(args, sizeof(char*)*(numargs+1));  // WHAT THE FUCK 
-		args[numargs] = NULL;
-		
-		/* WTF */
-		
-		// fork it.  fork it all.
-		pid_t pid;
-		int status;
-		switch (pid = fork()) {
-			case 0:
-				if (rinflag) {
-					fdin = open(rin_fname, O_RDONLY);
-					close(STDIN_FILENO);
-					dup2(fdin, STDIN_FILENO);
-					close(fdin);
-				}
-				if (routflag) {
-					fdout = open(rout_fname, O_WRONLY, 0644);
-					close(STDOUT_FILENO);
-					dup2(fdout, STDOUT_FILENO);
-					close(fdout);
-				}
-				printf("child starting\n");
-				execvp(args[0], args);
-				return 0;
-				break;
-			case -1:
-				printf("ERROR: could not create new process");
-				goto startloop;
-				break;
-			default:
-				waitpid(pid, &status, 0); 
-				break;
-		}
-		
-		
-		for (i=0; i < numargs; i++)
-			free(args[i]);
-		free(args);
-		args = 0;
+		free(pfds);
 	}
 	
 end:
 	free(tokens);
 	free(rin_fname);
 	free(rout_fname);
+	for (i=0 ; i <= numargs; i++) {
+		free(args[i]);
+	}
+	free(args);
 	
 	return 0;
 }
